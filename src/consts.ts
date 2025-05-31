01b7f0277950b47b34c419e76ead3889c0b2177d
Danny White <3104761+dnywh@users.noreply.github.com>
2025-05-31 15:06:20 +1000

    Site consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,21 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Danny’s Site";
+export const SITE_DESCRIPTION = "Beagles are bad";
+
+export const SITE = {
+  NAME: "Danny’s Site",
+  DESCRIPTION: "Beagles are good",
+};
+
+export const SOCIALS = [
+  {
+    NAME: "github",
+    HREF: "https://github.com/dnywh",
+  },
+  {
+    NAME: "linkedin",
+    HREF: "https://www.linkedin.com/in/dnywh",
+  },
+];
